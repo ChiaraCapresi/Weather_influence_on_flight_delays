62670e95c4618d6e9f6441a34c7d44f443083ca6
ChiaraCapresi <chiara.capresi@yahoo.it>
2023-08-16 17:56:03 +0200

    first day

---
+++ exploration_and_cleaning/data_cleaning.R
@@ -0,0 +1,35 @@
+# Data Cleaning
+
+library(tidyverse)
+
+
+airlines <- read_csv(here::here("raw_data/airlines.csv"))
+airports <- read_csv(here::here("raw_data/airports.csv"))
+flights <- read_csv(here::here("raw_data/flights.csv"))
+planes <- read_csv(here::here("raw_data/planes.csv"))
+weather <- read_csv(here::here("raw_data/weather.csv"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
